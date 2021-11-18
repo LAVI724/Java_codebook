6a4f38d45b84ecad85919b4dd429534a58283475
LAVI_0724 <83853006+LAVI724@users.noreply.github.com>
2021-11-19 01:25:28 +0800

    upload

---
+++ Contents/chapter_practice/ch9_0x9.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    static class RegularPolygon{
+        private int n = 3;
+        private double side = 1, x = 0, y = 0;
+        
+        public static double getPerimeter(){
+            
+        }
+        public static double getArea(){
+            
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
